661807f56c2354dc90e8e9af4a892c535e6f970b
BagelsForAll <distinguishedbagel@gmail.com>
2018-12-30 14:42:25 +0000

    AudioSwing
    Plays a "Swing" sound effect. Provides a placeholder for said audio to be determined. Used as an Animation Event.

---
+++ AudioSwing.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSwing : MonoBehaviour {
+
+    public AudioClip clip;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        PlaySwingAudio();
+    }
+
+    public void PlaySwingAudio ()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.PlayOneShot(clip, 0.5F);
+    }
+
+}
