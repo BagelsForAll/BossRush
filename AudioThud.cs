50abe99bba812209b5a48d4cc4f3bd73894b2f1e
BagelsForAll <distinguishedbagel@gmail.com>
2018-12-30 14:42:59 +0000

    AudioThud
    Plays a "Thud" sound effect. Provides a placeholder for said audio to be determined. Used as an Animation Event.

---
+++ AudioThud.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioThud : MonoBehaviour
+{
+
+    public AudioClip clip;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        PlayThudAudio();
+    }
+
+    public void PlayThudAudio()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.PlayOneShot(clip, 1F);
+    }
+
+}
