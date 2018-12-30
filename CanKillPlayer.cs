c25a2fd9c4c5657420e7cd22eb73a0f87bdd979e
BagelsForAll <distinguishedbagel@gmail.com>
2018-12-30 14:48:06 +0000

    CanKillPlayer
    When applied to a game object, this allows said object to kill the player and trigger a "Game Over" on collision.

---
+++ CanKillPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CanKillPlayer : MonoBehaviour {
+
+    public GameManager myGameManager;
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        { // check if it's the player, if you want
+            EndTheGame();
+            Destroy(other.gameObject);
+        }
+    }
+
+    public void EndTheGame()
+    {
+        myGameManager.EndGame();
+    }
+}
