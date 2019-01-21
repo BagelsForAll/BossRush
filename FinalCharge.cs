dfaf317b8c7e2eb51556f7c2b4725d932b7ffd35
BagelsForAll <distinguishedbagel@gmail.com>
2019-01-21 09:56:15 +0000

    FinalCharge
    Plays audio on the boss' final attack.

---
+++ FinalCharge.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalCharge : MonoBehaviour
+{
+
+    public AudioClip charge;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        PlayFinalAttack();
+    }
+
+    public void PlayFinalAttack()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.PlayOneShot(charge, 0.7f);
+    }
+}
