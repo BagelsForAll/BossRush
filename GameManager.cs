e270787f1c7b41f0203ff1e2a7963405f664fa69
BagelsForAll <distinguishedbagel@gmail.com>
2019-01-21 09:56:38 +0000

    GameManager
    Manages the loading of death and victory scenes.

---
+++ GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+    public GameObject music;
+    public GameObject healthbar;
+
+    public void StartGame()
+    {
+        healthbar.SetActive(true);
+    }
+
+    public void EndGame()
+    {
+        Debug.Log("Game Over");
+        Destroy(music);
+        Time.timeScale = 0;
+        SceneManager.LoadScene("DeathScreen", LoadSceneMode.Additive);
+    }
+
+    public void Victory()
+    {
+        Debug.Log("Victory!");
+        Destroy(music);
+        SceneManager.LoadScene("VictoryScreen", LoadSceneMode.Single);
+    }
+}
