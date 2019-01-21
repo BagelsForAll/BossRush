aec29b98f1cf4e92c2dd92ee40dfd6f82e18bbbd
BagelsForAll <distinguishedbagel@gmail.com>
2019-01-21 09:57:21 +0000

    LoadBoss
    Loads the boss fight and intro cutscene.

---
+++ LoadBoss.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadBoss : MonoBehaviour
+{
+    public void Update()
+    {
+        if (Input.GetKey(KeyCode.KeypadEnter))
+        {
+            LoadBossLevel();
+        }
+    }
+    public void LoadBossLevel()
+    {
+        // Load the boss fight and video
+        SceneManager.LoadScene("Boss Rush v1", LoadSceneMode.Single);
+    }
+}
